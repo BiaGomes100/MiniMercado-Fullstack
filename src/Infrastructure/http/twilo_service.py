069dac5a098185d7ca653ea6fac9235515a98795
BiaGomes100 <bia44465@gmail.com>
2025-09-22 02:02:48 -0300

    ainda ajustando credenciais

--- src/Infrastructure/http/twilo_service.py
+++ src/Infrastructure/http/twilo_service.py
@@ -1,7 +1,6 @@
-from flask.cli import load_dotenv
+from dotenv import load_dotenv  # Mude este import
 from twilio.rest import Client
 import os
-
 
 load_dotenv()
 
@@ -10,6 +9,9 @@ class TwilioService:
         self.account_sid = os.getenv("TWILIO_ACCOUNT_SID")
         self.auth_token = os.getenv("TWILIO_AUTH_TOKEN")
         self.from_number = os.getenv("TWILIO_WHATSAPP_NUMBER")
+        # Verifique se as variáveis foram carregadas
+        if not all([self.account_sid, self.auth_token, self.from_number]):
+            raise ValueError("Variáveis de ambiente do Twilio não configuradas corretamente.")
         self.client = Client(self.account_sid, self.auth_token)
 
     def enviar_mensagem(self, to_number, mensagem):
