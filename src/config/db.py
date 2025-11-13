d3f17fb36060f8e56a6303684afa8b2f92468871
BiaGomes100 <bia44465@gmail.com>
2025-11-12 21:00:06 -0300

    atualizando banco

--- src/config/db.py
+++ src/config/db.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:191077@localhost/mini_mercado"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
